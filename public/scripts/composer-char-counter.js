c12626256696be50fc820e6c0aa1ad8763bbce9f
David Morgan <david.alexandermorgan@gmail.com>
2018-08-14 22:54:45 +0000

    refactored jquery to lower the character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("textarea[name*='text']").keyup(function(event){
-        let counter = $(this).parent().find(".counter");
+        let counter = $(this).siblings(".counter");
         let remaining = 140 - $(this).val().length
 
         counter.text(remaining)  
